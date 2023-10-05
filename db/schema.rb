2cb291e2ff076b794b1cc0645021f02abc61be38
cancelei <glauber.bannwart@gmail.com>
2023-10-05 17:34:27 -0300

    add posts counter to user's table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_05_184004) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_05_203245) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,11 +34,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_05_184004) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
     t.string "email"
     t.string "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.text "bio"
+    t.integer "posts_counter", default: 0
   end
 
   add_foreign_key "comments", "posts"
