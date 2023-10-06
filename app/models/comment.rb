e4dab13fbd80bcf3a674e78406b80d0628076aaf
cancelei <glauber.bannwart@gmail.com>
2023-10-06 16:36:29 -0300

    update comment model to increment/decrease #

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,10 +2,15 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  after_create :update_post_comments_counter
+  after_create :increment_post_comments_counter
+  after_destroy :decrement_post_comments_counter
 
-  def update_post_comments_counter
+  def increment_post_comments_counter
     post.increment!(:comments_counter)
+  end
+
+  def decrement_post_comments_counter
+    post.decrement!(:comments_counter)
   end
 
   validates :content, presence: true
