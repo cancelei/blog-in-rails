8c80af37658ead9b7c6aed662ecf3f3b032f5e3b
cancelei <glauber.bannwart@gmail.com>
2023-10-06 11:01:04 -0300

    Update Models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  after_create :update_post_comments_counter
+
+  def update_post_comments_counter
+    post.increment!(:comments_counter)
+  end  
 end
