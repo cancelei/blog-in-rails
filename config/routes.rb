02f42689500dd8931f49e812adea1e3b2b54c45e
cancelei <glauber.bannwart@gmail.com>
2023-10-10 12:27:37 -0300

    Set up routes for users and users/posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'users#index' # this will map the "https://<your-domain>/" URL
+  
+  get 'users/:id', to: 'users#show', as: 'user'
+  get 'users/:user_id/posts', to: 'posts#index'
+  get 'users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
 end
