25e658735cbda47c1cef9a97c4315dd93d1efdc1
cancelei <glauber.bannwart@gmail.com>
2023-10-10 18:47:36 -0300

    Update describe blocks in controler specs

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,20 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /' do
-    it 'renders the index template and has correct status' do
+
+  describe 'GET /index' do
+    it 'renders the index template' do
       get root_path
-      expect(response).to have_http_status(200)
       expect(response).to render_template(:index)
-      expect(response.body).to include('List of all users')
     end
 
-    it 'renders the show template and has correct status for a specific user' do
-      user = User.create!(email: 'test@example.com', name: 'Test User', bio: 'Test bio.')
+    it 'has a status of 200' do
+      get root_path
+      expect(response).to have_http_status(200)
+    end
+
+    it 'displays a list of all users' do
+      get root_path
+      expect(response.body).to include('List of all users')
+    end
+  end
+
+  describe 'GET /show' do
+    let(:user) { User.create!(email: 'test@example.com', name: 'Test User', bio: 'Test bio.') }
+
+    it 'renders the show template' do
+      get user_path(user.id)
+      expect(response).to render_template(:show)
+    end
+
+    it 'has a status of 200' do
       get user_path(user.id)
       expect(response).to have_http_status(200)
-      expect(response).to render_template(:show)
+    end
+
+    it 'displays the name of the user' do
+      get user_path(user.id)
       expect(response.body).to include(user.name)
     end
   end
+
 end
