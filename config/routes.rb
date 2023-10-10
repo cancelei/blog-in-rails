427b606bc572bafe16891e5d0628f52fab5c90d9
cancelei <glauber.bannwart@gmail.com>
2023-10-10 15:43:23 -0300

    update routes for better readability

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'users#index' # this will map the "https://<your-domain>/" URL
-  
-  get 'users/:id', to: 'users#show', as: 'user'
-  get 'users/:user_id/posts', to: 'posts#index'
-  get 'users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
+  root to: 'users#index'
+
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 end
