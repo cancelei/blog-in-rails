25c836d6c9d021c0ed98e56c2f0af986febe80a9
cancelei <glauber.bannwart@gmail.com>
2023-10-19 02:45:36 -0300

    DB migration, seed and schema updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_06_172429) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_19_041125) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,7 +45,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_06_172429) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
     t.string "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
