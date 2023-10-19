65a43d4b492d61555f03671e1d67beb6ce1c5a64
najibullah <najib2020202020@gmail.com>
2023-10-19 10:41:49 +0430

    Solve n+1 problem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_06_172429) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_15_002924) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,6 +41,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_06_172429) do
     t.datetime "updated_at", null: false
     t.integer "comments_counter", default: 0
     t.integer "likes_counter", default: 0
+    t.text "text"
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
