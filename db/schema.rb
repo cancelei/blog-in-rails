4140fbb4c27a922c43ff1818079c9167bf3e5ca6
Joseph Lwanzo Khausi <josephlkausi@gmail.com>
2023-10-19 18:13:26 +0200

    rebase true

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_15_002924) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_19_041125) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
