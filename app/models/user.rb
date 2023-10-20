6bc648a5ef8249500d8f07ef7cbd94d2955a07fc
cancelei <glauber.bannwart@gmail.com>
2023-10-20 19:35:38 -0300

    Update views to display 'delete' button

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  # Callbacks
+  before_create :set_default_role
+  # Associations
   devise :database_authenticatable, :registerable, :confirmable, :recoverable, :rememberable, :validatable
   has_many :posts, foreign_key: 'author_id'
   has_many :comments
@@ -7,8 +10,15 @@ class User < ApplicationRecord
   # Validations
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :role, inclusion: { in: %w[user admin], message: '%<value>s is not a valid role' }
   # Methods
   def recent_posts
     posts.order(created_at: :desc).limit(3)
+  end
+
+  private
+
+  def set_default_role
+    self.role ||= 'user'
   end
 end
