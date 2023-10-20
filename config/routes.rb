8be4c6861426c1c9bce3c721bf86c448df885290
Joseph Lwanzo Khausi <josephlkausi@gmail.com>
2023-10-20 15:23:09 +0200

    Add authenticated user case

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do
-    root to: 'devise/sessions#new'
+    authenticated :user do
+      root to: 'users#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root to: 'devise/sessions#new', as: :unauthenticated_root
+    end
   end
   
   resources :users, only: [:index, :show] do
