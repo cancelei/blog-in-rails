649916cb1e137c40b8f6ccd51f35da0b58f6ff0b
cancelei <glauber.bannwart@gmail.com>
2023-10-20 13:54:21 -0300

    update devise settings

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :confirmable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :confirmable, :recoverable, :rememberable, :validatable, :confirmable
   has_many :posts, foreign_key: 'author_id'
   has_many :comments
   has_many :likes
@@ -7,7 +7,6 @@ class User < ApplicationRecord
   # Validations
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  devise :confirmable
   # Methods
   def recent_posts
     posts.order(created_at: :desc).limit(3)
