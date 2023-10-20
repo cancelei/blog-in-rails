2ed3c61bea30c9bee098cb4204391f1930f567c5
rails <najib2020202020@gmail.com>
2023-10-21 00:49:22 +0430

    Update the ablility.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,9 +6,9 @@ class Ability
   def initialize(user)
     # Define abilities for the user here. For example:
     #
-    #   return unless user.present?
+      return unless user.present?
     #   can :read, :all
-    #   return unless user.admin?
+      return unless user.admin?
     #   can :manage, :all
     #
     # The first argument to `can` is the action you are giving the user
